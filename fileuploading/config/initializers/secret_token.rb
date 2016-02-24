2664a3b8223eac5c41f07c3913569fa6a1b21962
Anuradha2016 <anuradh2015@gmail.com>
2016-02-24 16:57:34 +0530

    Add fileuploading

---
+++ fileuploading/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fileuploading::Application.config.secret_token = '4908984c30332f18d6079a8e0cfd1b6ca7f4403d770a23467d64cd7a68bd1d113866b3a979128e1f72c3dce7c7619e29d2a82a3c499f156effbf6074440d585c'
