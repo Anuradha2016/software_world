c1b48d083843efeeb372def9b7b25feadc333168
Anuradha2016 <anuradh2015@gmail.com>
2016-03-17 16:57:58 +0530

    updating without_db

--- without_db/app/controllers/without_controller.rb
+++ without_db/app/controllers/without_controller.rb
@@ -1,16 +1,19 @@
 class WithoutController < ApplicationController
   def new
    render :action =>'new'
-    
   end
  def edit
    render :action =>'edit'
-    
  end
 def paint
    render :action =>'paint'
-    
  end
+ def spark
+   render :action =>'spark'
+ end
+def game
+render :action => 'game'
+end
 def slide
 render :action => 'slide'
 end
@@ -20,11 +23,17 @@ end
 def orderfile
   render :action => 'orderfile'
 end
+ def bunny
+   render :action =>'bunny'
+  end
 def leave
   render :action => 'leave'
 end
 def leaf
   render :action => 'leaf'
+end
+def rain
+  render :action => 'rain'
 end
 
 def slider
